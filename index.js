8cd7c97904c074c6957b45e94933c06a4a40c467
MrNghia123 <nghia.huynh.duc@gmail.com>
2023-06-01 08:34:26 +0700

    export both cloneable and Cloneable

--- index.js
+++ index.js
@@ -1,170 +1,122 @@
 'use strict'
-
-const { PassThrough } = require('readable-stream')
-const { nextTick } = require('process')
-
-function Cloneable (stream, opts) {
-  if (!(this instanceof Cloneable)) {
-    return new Cloneable(stream, opts)
+const { PassThrough } = require("stream")
+class Cloneable extends PassThrough {
+  constructor(stream, opts) {
+    super({...opts, objectMode: stream._readableState.objectMode})
+    this._original = stream
+    this._clonesCount = 1
+    this._internalPipe = false
+    forwardDestroy(stream, this)
+    this.on('newListener', this.onData)
+    this.on("resume", this.onResume)
+    this._hasListener = true
   }
-
-  const objectMode = stream._readableState.objectMode
-  this._original = stream
-  this._clonesCount = 1
-  this._internalPipe = false
-
-  opts = opts || {}
-  opts.objectMode = objectMode
-
-  PassThrough.call(this, opts)
-
-  forwardDestroy(stream, this)
-
-  this.on('newListener', onData)
-  this.on('resume', onResume)
-
-  this._hasListener = true
-}
-
-Object.setPrototypeOf(Cloneable.prototype, PassThrough.prototype)
-Object.setPrototypeOf(Cloneable, PassThrough)
-
-function onData (event, listener) {
-  if (event === 'data' || event === 'readable') {
-    this._hasListener = false
-    this.removeListener('newListener', onData)
-    this.removeListener('resume', onResume)
-
-    nextTick(clonePiped, this)
+  _destroy (err, cb) {
+    if (!err) {
+      this.push(null)
+      this.end()
+    }
+    process.nextTick(cb, err)
+  }
+  resume() {
+    if (this._internalPipe) return
+    super.resume()
+  }
+  clone() {
+    if (!this._original) {
+      throw new Error('already started')
+    }
+    this._clonesCount++
+    // the events added by the clone should not count
+    // for starting the flow
+    this.removeListener('newListener', this.onData)
+    this.removeListener('resume', this.onResume)
+    const clone = new Clone(this)
+    if (this._hasListener) {
+        this.on('newListener', this.onData)
+        this.on('resume', this.onResume)
+    }
+    return clone
+  }
+  onData (event) {
+      if (event === 'data' || event === 'readable') {
+          this._hasListener = false
+          this.removeListener('newListener', this.onData)
+          this.removeListener('resume', this.onResume)
+        process.nextTick(clonePiped, this)
+      }
+  }
+  onResume () {
+      this._hasListener = false
+      this.removeListener('newListener', this.onData)
+      this.removeListener('resume', this.onResume)
+      process.nextTick(clonePiped, this)
+  }    
+  static isCloneable (stream) {
+      return stream instanceof Cloneable || stream instanceof Clone
   }
 }
-
-function onResume () {
-  this._hasListener = false
-  this.removeListener('newListener', onData)
-  this.removeListener('resume', onResume)
-
-  nextTick(clonePiped, this)
-}
-
-Cloneable.prototype.resume = function () {
-  if (this._internalPipe) return
-  PassThrough.prototype.resume.call(this)
-}
-
-Cloneable.prototype.clone = function () {
-  if (!this._original) {
-    throw new Error('already started')
-  }
-
-  this._clonesCount++
-
-  // the events added by the clone should not count
-  // for starting the flow
-  this.removeListener('newListener', onData)
-  this.removeListener('resume', onResume)
-  const clone = new Clone(this)
-  if (this._hasListener) {
-    this.on('newListener', onData)
-    this.on('resume', onResume)
-  }
-
-  return clone
-}
-
-Cloneable.prototype._destroy = function (err, cb) {
-  if (!err) {
-    this.push(null)
-    this.end()
-  }
-
-  nextTick(cb, err)
-}
-
 function forwardDestroy (src, dest) {
   src.on('error', destroy)
   src.on('close', onClose)
-
   function destroy (err) {
     src.removeListener('close', onClose)
     dest.destroy(err)
   }
-
   function onClose () {
     dest.end()
   }
 }
-
 function clonePiped (that) {
   if (--that._clonesCount === 0 && !that._readableState.destroyed) {
     that._original.pipe(that)
     that._original = undefined
   }
 }
-
-function Clone (parent, opts) {
-  if (!(this instanceof Clone)) {
-    return new Clone(parent, opts)
+class Clone extends PassThrough {
+  constructor(parent, opts) {
+    super({...opts, objectMode: parent._readableState.objectMode})
+    this.parent = parent
+    forwardDestroy(parent, this)
+    // setting _internalPipe flag to prevent this pipe from starting
+    // the flow. we have also overridden resume to do nothing when
+    // this pipe tries to start the flow
+    parent._internalPipe = true
+    parent.pipe(this)
+    parent._internalPipe = false
+    // the events added by the clone should not count
+    // for starting the flow
+    // so we add the newListener handle after we are done
+    this.on('newListener', this.onDataClone)
+    this.once('resume', this.onResumeClone)
   }
-
-  const objectMode = parent._readableState.objectMode
-
-  opts = opts || {}
-  opts.objectMode = objectMode
-
-  this.parent = parent
-
-  PassThrough.call(this, opts)
-
-  forwardDestroy(parent, this)
-
-  // setting _internalPipe flag to prevent this pipe from starting
-  // the flow. we have also overridden resume to do nothing when
-  // this pipe tries to start the flow
-  parent._internalPipe = true
-  parent.pipe(this)
-  parent._internalPipe = false
-
-  // the events added by the clone should not count
-  // for starting the flow
-  // so we add the newListener handle after we are done
-  this.on('newListener', onDataClone)
-  this.once('resume', onResumeClone)
-}
-
-function onDataClone (event, listener) {
-  // We start the flow once all clones are piped or destroyed
-  if (event === 'data' || event === 'readable' || event === 'close') {
-    nextTick(clonePiped, this.parent)
-    this.removeListener('newListener', onDataClone)
-    this.removeListener('resume', onResumeClone)
+  clone () {
+    return this.parent.clone()
+  }
+  _destroy (err, cb) {
+    if (!err) {
+      this.push(null)
+      this.end()
+    }
+    process.nextTick(cb, err)
+  }   
+  onDataClone (event) {
+    // We start the flow once all clones are piped or destroyed
+    if (event === 'data' || event === 'readable' || event === 'close') {
+      process.nextTick(clonePiped, this.parent)
+      this.removeListener('newListener', this.onDataClone)
+      this.removeListener('resume', this.onResumeClone)
+    }
+  }
+    
+  onResumeClone () {
+    this.removeListener('newListener', this.onDataClone)
+    this.removeListener('resume', this.onResumeClone)
+    process.nextTick(clonePiped, this.parent)
   }
 }
-
-function onResumeClone () {
-  this.removeListener('newListener', onDataClone)
-  this.removeListener('resume', onResumeClone)
-  nextTick(clonePiped, this.parent)
+function cloneable(stream, opts) {
+  return new Cloneable(stream, opts)
 }
-
-Object.setPrototypeOf(Clone.prototype, PassThrough.prototype)
-Object.setPrototypeOf(Clone, PassThrough)
-
-Clone.prototype.clone = function () {
-  return this.parent.clone()
-}
-
-Cloneable.isCloneable = function (stream) {
-  return stream instanceof Cloneable || stream instanceof Clone
-}
-
-Clone.prototype._destroy = function (err, cb) {
-  if (!err) {
-    this.push(null)
-    this.end()
-  }
-
-  nextTick(cb, err)
-}
-
-module.exports = Cloneable
+const myModule = module.exports = cloneable
+myModule.Cloneable = Cloneable
